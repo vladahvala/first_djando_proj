d91f94c8a59510fa7aaaadd6b62df2bc79b38749
vladahvala <80634246+vladahvala@users.noreply.github.com>
2022-12-12 18:47:52 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+#дані збираються з бази даних, відправляються на html-сторінку, потім рендеряться(готуються) і відправляються назад користувачу
 
 def blog_main(request, *args):
-    return render(request, 'blog_main.html', {})
+    posts = Post.objects.all()
+    data_dict = {
+        "posts": posts
+    }
+    return render(request, 'blog_main.html', data_dict)#запит, адреса, словник із даними
