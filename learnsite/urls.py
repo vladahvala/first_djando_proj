71145f2a7afd5a015a4c5c9e97cdc5dad0414e64
vladahvala <80634246+vladahvala@users.noreply.github.com>
2022-12-12 18:49:25 +0200

    Update urls.py

--- learnsite/urls.py
+++ learnsite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls'))#якщо введено blog далі маршрутизатор звертатитметься до наступного файлу urls.py
 ]
 
 #'admin/', - шаблон відносного url,  
